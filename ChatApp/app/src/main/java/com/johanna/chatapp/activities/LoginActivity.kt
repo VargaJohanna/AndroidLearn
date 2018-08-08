f3c1c66807ce539556c538d2b2bde2df8bb566c3
VargaJohanna <varga.johanna@gmail.com>
2018-08-08 23:55:07 +0100

    Create login card view

--- ChatApp/app/src/main/java/com/johanna/chatapp/activities/LoginActivity.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/activities/LoginActivity.kt
@@ -1,12 +1,13 @@
- package com.johanna.chatapp
+package com.johanna.chatapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.johanna.chatapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
