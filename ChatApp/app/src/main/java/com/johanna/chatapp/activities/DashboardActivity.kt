cf9509394ab8adc77b47e7203d175f423d191a16
VargaJohanna <varga.johanna@gmail.com>
2018-08-11 22:16:00 +0100

    Create account and save to Firebase database

---
+++ ChatApp/app/src/main/java/com/johanna/chatapp/activities/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.johanna.chatapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.johanna.chatapp.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
--- ChatApp/app/src/main/java/com/johanna/chatapp/activities/MainActivity.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/activities/MainActivity.kt
@@ -1,13 +1,19 @@
  package com.johanna.chatapp.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.johanna.chatapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createAccountButton.setOnClickListener{
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+        }
     }
 }
