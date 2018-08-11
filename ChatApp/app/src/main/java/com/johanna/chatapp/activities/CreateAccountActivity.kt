7878078e18e1742652e9b816ea689452ae78a67d
VargaJohanna <varga.johanna@gmail.com>
2018-08-11 22:20:22 +0100

    Clean up create account activity

--- ChatApp/app/src/main/java/com/johanna/chatapp/activities/CreateAccountActivity.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/activities/CreateAccountActivity.kt
@@ -1,8 +1,8 @@
-    package com.johanna.chatapp.activities
+package com.johanna.chatapp.activities
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.widget.Toast
 import com.google.android.gms.tasks.Task
@@ -12,10 +12,11 @@ import com.google.firebase.database.FirebaseDatabase
 import com.johanna.chatapp.R
 import kotlinx.android.synthetic.main.activity_create_account.*
 
-    class CreateAccountActivity : AppCompatActivity() {
-        private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+class CreateAccountActivity : AppCompatActivity() {
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val mDataBase = FirebaseDatabase.getInstance().reference
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
@@ -24,51 +25,47 @@ import kotlinx.android.synthetic.main.activity_create_account.*
             val email = accountEmailEt.text.toString().trim()
             val password = accountPasswordEt.text.toString().trim()
 
-            if(!TextUtils.isEmpty(displayName)
-            || !TextUtils.isEmpty(email)
-            || !TextUtils.isEmpty(password)) {
+            if (!TextUtils.isEmpty(displayName)
+                    || !TextUtils.isEmpty(email)
+                    || !TextUtils.isEmpty(password)) {
                 createAccount(email, password, displayName)
             } else {
                 Toast.makeText(this, "Please fill out the fields", Toast.LENGTH_SHORT).show()
             }
         }
     }
-        fun createAccount(email: String, password: String, displayName:String) {
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener {
-                        task: Task<AuthResult> ->
-                        if(task.isSuccessful) {
-                            val currentUser = mAuth.currentUser
-                            val userId = currentUser?.uid
 
-                            val mDataBase = FirebaseDatabase.getInstance().reference
-                                    .child("Users").child(userId!!)
+    fun createAccount(email: String, password: String, displayName: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task: Task<AuthResult> ->
+                    if (task.isSuccessful) {
+                        val currentUser = mAuth.currentUser
+                        val userId = currentUser?.uid
+                        val userObject = HashMap<String, String>()
+                        mDataBase.child("Users").child(userId!!)
 
+                        userObject.put("display_name", displayName)
+                        userObject.put("status", "Hello")
+                        userObject.put("image", "default")
+                        userObject.put("thumb_image", "default")
 
-                            val userObject = HashMap<String, String>()
-                            userObject.put("display_name", displayName)
-                            userObject.put("status", "Hello")
-                            userObject.put("image", "default")
-                            userObject.put("thumb_image", "default")
+                        mDataBase.setValue(userObject).addOnCompleteListener { task: Task<Void> ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "User created!", Toast.LENGTH_SHORT).show()
+                                val dashboardActivity = Intent(this, DashboardActivity::class.java)
+                                dashboardActivity.putExtra("name", displayName)
+                                startActivity(dashboardActivity)
+                                finish()
+                            } else {
+                                Toast.makeText(this, "User not created!", Toast.LENGTH_SHORT).show()
 
-                            mDataBase.setValue(userObject).addOnCompleteListener{
-                                task: Task<Void> ->
-                                if(task.isSuccessful){
-                                    Toast.makeText(this, "User created!", Toast.LENGTH_SHORT).show()
-                                    val dashboardActivity = Intent(this, DashboardActivity::class.java)
-                                    dashboardActivity.putExtra("name", displayName)
-                                    startActivity(dashboardActivity)
-                                    finish()
-                                } else {
-                                    Toast.makeText(this, "User not created!", Toast.LENGTH_SHORT).show()
-
-                                }
                             }
                         }
                     }
-                    .addOnFailureListener{
-                        Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
-                    }
-        }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                }
+    }
 }
 
