081764bd9b98066c3272f41c388e927dfdced6f5
VargaJohanna <varga.johanna@gmail.com>
2018-08-11 23:11:30 +0100

    Login only once

--- ChatApp/app/src/main/java/com/johanna/chatapp/activities/MainActivity.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/activities/MainActivity.kt
@@ -3,14 +3,29 @@
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.johanna.chatapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
+     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+     private var mAuthListener: FirebaseAuth.AuthStateListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            firebaseAuth: FirebaseAuth ->
+            val user = firebaseAuth.currentUser
+
+            if(user != null) {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Need to login", Toast.LENGTH_LONG).show()
+            }
+        }
 
         createAccountButton.setOnClickListener{
             startActivity(Intent(this, CreateAccountActivity::class.java))
@@ -20,4 +35,16 @@ import kotlinx.android.synthetic.main.activity_main.*
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
+
+     override fun onStart() {
+         super.onStart()
+         mAuth.addAuthStateListener(mAuthListener!!)
+     }
+
+     override fun onStop() {
+         super.onStop()
+         if(mAuthListener != null) {
+             mAuth.removeAuthStateListener(mAuthListener!!)
+         }
+     }
 }
