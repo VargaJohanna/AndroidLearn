e67d27250b5a54f112f1a522fd9d8c98a4673d7c
VargaJohanna <varga.johanna@gmail.com>
2018-09-02 18:41:56 +0100

    Minor fixes in Profile

--- ChatApp/app/src/main/java/com/johanna/chatapp/profile/ProfileActivity.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/profile/ProfileActivity.kt
@@ -8,13 +8,13 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity(), ProfileView {
-    private val profilePresenter = ProfilePresenter(this)
-    lateinit var userIdValue: String
+    private val profilePresenter by lazy {ProfilePresenter(userIdValue, this)}
+    private lateinit var userIdValue: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        supportActionBar?.title = "Profile"
+        supportActionBar?.title = this.getString(R.string.profile_tab)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         userIdValue = intent.extras.get(userId).toString()
         profilePresenter.setUpProfile()
--- ChatApp/app/src/main/java/com/johanna/chatapp/profile/ProfilePresenter.kt
+++ ChatApp/app/src/main/java/com/johanna/chatapp/profile/ProfilePresenter.kt
@@ -4,11 +4,11 @@ import android.util.Log
 import com.google.firebase.database.*
 import com.johanna.chatapp.Database
 
-class ProfilePresenter constructor(private val profileActivity: ProfileActivity) {
+class ProfilePresenter constructor(private val userIdValue: String, private val profileView: ProfileView) {
     private val userReference: DatabaseReference by lazy {
         FirebaseDatabase.getInstance().reference
                 .child(Database.usersNode)
-                .child(profileActivity.userIdValue)
+                .child(userIdValue)
     }
 
     fun setUpProfile() {
@@ -18,14 +18,12 @@ class ProfilePresenter constructor(private val profileActivity: ProfileActivity)
                 val status = dataSnapshot.child("status").value.toString()
                 val image = "https://api.adorable.io/avatars/260/$status.png"
 
-                profileActivity.linkProfileDetails(displayName, status, image)
+                profileView.linkProfileDetails(displayName, status, image)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 Log.e("Error", databaseError.message)
-
             }
         })
     }
-
 }
